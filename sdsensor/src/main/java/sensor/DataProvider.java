3e3e4d5807a61cb3c96617b87703b9be27f19392
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-10-14 13:47:47 +0200

    continuous sensor

--- sdsensor/src/main/java/sensor/DataProvider.java
+++ sdsensor/src/main/java/sensor/DataProvider.java
@@ -6,60 +6,81 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import org.apache.log4j.Logger;
 
 import sensor.GenericDataInstance.Record;
 
-public class DataProvider {
+public class DataProvider implements Runnable {
 
-	public static DataProvider provider;
-	private static final Object lock = new Object();
+    public static DataProvider provider;
+    private static final Object lock = new Object();
+    private static Logger LOGGER = Logger.getLogger(DataProvider.class);
 
-	public static DataProvider getProvider() {
-		synchronized (lock) {
-			if (provider == null) {
-				provider = new DataProvider();
-			}
-			return provider;
-		}
-	}
+    static {
+        provider = new DataProvider();
+    }
 
-	private Stack<GenericDataInstance> dataInstances = new Stack<GenericDataInstance>();
+    public static DataProvider getProvider() {
+////        synchronized (lock) {
+//            if (provider == null) {
+//                provider = new DataProvider();
+//            }
+        return provider;
+//        }
+    }
+    private Stack<GenericDataInstance> dataInstances = new Stack<GenericDataInstance>();
 
-	private DataProvider() {
-		//Allowed format of the .csv file is
-		//SENSORID, DATA_INSTANCE_NAME1, DATA_INSTANCE_NAME2, ... DATA_INSTANCE_NAMEN
-		//1278, 123, 123, abc ... 567
-		
-		InputStreamReader reader = new InputStreamReader(DataProvider.class.getClassLoader().getResourceAsStream("data.csv"));
-		BufferedReader br = new BufferedReader(reader);
+    public void run() {
 
-		try {
-			
-			String firstLine = br.readLine();
-			String headers[] = firstLine.split(",");
-						
-			String line;
-			while ((line = br.readLine()) != null) {
-				String[] split = line.split(",");
-				List<Record> records = new ArrayList<Record>();
-				for (int i = 1;i < split.length; i++){
-					Record r = new Record(headers[i],split[i]);
-					records.add(r);
-				}
-				GenericDataInstance ginst = new GenericDataInstance (split[0],records);
-				
-				dataInstances.push(ginst);
-			}
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        if (dataInstances.size() == 0) {
+            LOGGER.info(String.format("Reading csv file"));
+            InputStreamReader reader = new InputStreamReader(DataProvider.class.getClassLoader().getResourceAsStream("data.csv"));
+            BufferedReader br = new BufferedReader(reader);
 
-	}
+            try {
 
-	public GenericDataInstance getNextInstance() {
-		// FIXME: reading data in reverse order
-		return this.dataInstances.pop();
-	}
+                String firstLine = br.readLine();
+                String headers[] = firstLine.split(",");
 
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] split = line.split(",");
+                    List<Record> records = new ArrayList<Record>();
+                    for (int i = 1; i < split.length; i++) {
+                        Record r = new Record(headers[i], split[i]);
+                        records.add(r);
+                    }
+
+                    GenericDataInstance ginst = new GenericDataInstance(split[0], records);
+
+                    dataInstances.push(ginst);
+                }
+                br.close();
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage(), e);
+                e.printStackTrace();
+            }
+        } else {
+            LOGGER.info(String.format("Data stack not empty yet"));
+        }
+//            }
+//        };
+//        refreshDataTimer = new Timer(true);
+//        refreshDataTimer.schedule(task, 0, 1000);
+    }
+
+    private DataProvider() {
+    }
+     
+    public GenericDataInstance getNextInstance() {
+        // FIXME: reading data in reverse order
+        if (this.dataInstances.isEmpty()) {
+            return null;
+        } else {
+            return this.dataInstances.pop();
+        }
+    }
 }
--- sdsensor/src/main/java/sensor/LocationSensor.java
+++ sdsensor/src/main/java/sensor/LocationSensor.java
@@ -12,51 +12,57 @@ import sdapi.container.Bootstrapable;
 
 /**
  * @author Stefan Nastic (snastic@dsg.tuwien.ac.at)
- * 
+ *
  * Software-defined sensor
- * */
-
+ *
+ */
 public class LocationSensor implements Runnable, Bootstrapable {
 
-	private static Logger LOGGER = Logger.getLogger(LocationSensor.class);
-	private RefreshableProducerDelegate producerDelegate;
-	private ScheduledExecutorService scheduler = null;
-	private int updates = 1;
+    private static Logger LOGGER = Logger.getLogger(LocationSensor.class);
+    private RefreshableProducerDelegate producerDelegate;
+    private ScheduledExecutorService scheduler = null;
+    private int updates = 1;
 
-	public LocationSensor() {
-	}
+    public LocationSensor() {
+    }
 
-	public void setRootDependency(Object o) {
-		// do nothing
-	}
+    public void setRootDependency(Object o) {
+        // do nothing
+    }
 
-	public void start() {
-		LOGGER.info("Starting Location Sensor ...");
-		scheduler = Executors.newScheduledThreadPool(1);
-		scheduler.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
-	}
+    public void start() {
+        LOGGER.info("Starting Location Sensor ...");
+        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(DataProvider.getProvider(), 0, 5, TimeUnit.SECONDS);
 
-	public void run() {
-		GenericDataInstance temp = DataProvider.getProvider().getNextInstance();
-		LOGGER.info(String.format("Reading update number %s - %s ", this.updates, temp.getJSON()));
-		this.producerDelegate.push(new Event(temp.getJSON()));
-		updates++;
+    }
 
-	}
+    public void run() {
+        GenericDataInstance temp = DataProvider.getProvider().getNextInstance();
+        if (temp == null) {
+            LOGGER.info(String.format("Empty stack"));
+            return;
+        }
+        LOGGER.info(String.format("Reading update number %s - %s ", this.updates, temp.getJSON()));
+        this.producerDelegate.push(new Event(temp.getJSON()));
+        updates++;
 
-	public void setProducerDelegate(RefreshableProducerDelegate producer) {
-		this.producerDelegate = producer;
-	}
+    }
 
-	public void stop() {
-		LOGGER.info("Stoppinng Location Sensor ...");
-		if (scheduler != null) {
-			scheduler.shutdown();
-			this.producerDelegate.close();
-		}
+    public void setProducerDelegate(RefreshableProducerDelegate producer) {
+        this.producerDelegate = producer;
+    }
 
-	}
+    public void stop() {
+        LOGGER.info("Stoppinng Location Sensor ...");
+        if (scheduler != null) {
+            scheduler.shutdown();
+            this.producerDelegate.close();
+        }
 
-	public static void main(String[] args) {
-	}
+    }
+
+    public static void main(String[] args) {
+    }
 }
